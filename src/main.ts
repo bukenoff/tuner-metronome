3700137cd1e672ebfebce592a9827447e80ca7be
serik <bukenoff.s@gmail.com>
2021-01-08 18:42:58 +0700

    added styles, installed prime vue

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import 'primeflex/primeflex.css';
+import 'primevue/resources/themes/saga-blue/theme.css'; //theme
+import 'primevue/resources/primevue.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
 
 createApp(App)
+  .use(PrimeVue)
   .use(store)
   .use(router)
-  .mount("#app");
+  .mount('#app');
--- src/views/Metronome.vue
+++ src/views/Metronome.vue
@@ -1,20 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <h1>metronome</h1>
-
-    <div>
-      <div class="tempo">
-        <button type="button" @click="decreaseTempo">-</button>
+  <Card class="metronome-root">
+    <template #content>
+      <div class="metronome-tempo p-d-inline-flex p-ai-center p-jc-between">
+        <Button
+          class="p-button-secondary p-button-text"
+          icon="pi pi-minus"
+          @click="decreaseTempo"
+          :disabled="!is_stopped"
+        />
         <span>{{ tempo }}</span>
-        <button type="button" @click="increaseTempo">+</button>
+        <Button
+          class="p-button-secondary p-button-text"
+          icon="pi pi-plus"
+          @click="increaseTempo"
+          :disabled="!is_stopped"
+        />
       </div>
       <div class="control">
-        <button type="button" @click="togglePaused">
-          {{ is_stopped ? 'play' : 'stop' }}
-        </button>
-        <div>tap</div>
+        <Knob v-model="tempo" :min="30" :max="240" :disabled="!is_stopped" />
       </div>
+      <Button
+        :icon="is_stopped ? 'pi pi-play' : 'pi pi-pause'"
+        @click="togglePaused"
+      />
       <div class="beats">
         <div class="indicate">
           <div class="beat-item"></div>
@@ -22,22 +30,37 @@
           <div class="beat-item"></div>
           <div class="beat-item"></div>
         </div>
-        <div class="tempo">
-          4/4
+        <div class="metronome-time-signature">
+          <Dropdown
+            v-model="time_signature"
+            :options="time_signature_types"
+            optionLabel="name"
+            placeholder="Select a City"
+          />
         </div>
       </div>
-    </div>
-  </div>
+    </template>
+  </Card>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Button from 'primevue/button';
+import Card from 'primevue/card';
+import Knob from 'primevue/knob';
+import Dropdown from 'primevue/dropdown';
+
+const time_signature_types = [
+  { name: '4/4', value: '4/4' },
+  { name: '3/4', value: '3/4' },
+];
 
 interface IMetronomeType {
   tempo: number;
-  time_signature: '4/4' | '3/4';
+  time_signature: any;
   is_stopped: boolean;
   interval: null | ReturnType<typeof setInterval>;
+  time_signature_types: any[];
 }
 
 export default defineComponent({
@@ -45,18 +68,19 @@ export default defineComponent({
   data() {
     return {
       tempo: 90,
-      time_signature: '4/4',
+      time_signature: time_signature_types[0],
       is_stopped: true,
       interval: null,
+      time_signature_types,
     } as IMetronomeType;
   },
+  components: { Button, Card, Knob, Dropdown },
   methods: {
     tick() {
       const tick_sound = new Audio(require('../assets/audio/tick.wav'));
       tick_sound.play();
     },
     togglePaused() {
-      console.log('toggle paused works');
       if (!this.is_stopped && this.interval) {
         this.is_stopped = true;
         clearInterval(this.interval);
@@ -74,3 +98,18 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.metronome-root {
+  width: 300px;
+  margin: 0 auto;
+}
+
+.metronome-tempo {
+  width: 150px;
+}
+
+.metronome-time-signature {
+  margin-top: 0.5rem;
+}
+</style>

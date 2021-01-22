b5f301f54af1edd6b0072292157995dcba2f6e45
serik <bukenoff.s@gmail.com>
2021-01-22 19:41:04 +0700

    added click handlers to note buttons

--- src/views/Tuner.vue
+++ src/views/Tuner.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="home">
     <div class="uke_container">
-      <Button label="g" class="p-button-raised g-note note-button" />
-      <Button label="c" class="p-button-raised c-note note-button" />
-      <Button label="e" class="p-button-raised e-note note-button" />
-      <Button label="a" class="p-button-raised a-note note-button" />
+      <Button
+        label="g"
+        class="p-button-raised g-note note-button"
+        @click="playG"
+      />
+      <Button
+        label="c"
+        class="p-button-raised c-note note-button"
+        @click="playC"
+      />
+      <Button
+        label="e"
+        class="p-button-raised e-note note-button"
+        @click="playE"
+      />
+      <Button
+        label="a"
+        class="p-button-raised a-note note-button"
+        @click="playA"
+      />
       <img class="uke" src="../assets/ukulele.svg" alt="" />
     </div>
   </div>
@@ -13,9 +29,37 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Button from 'primevue/button';
+import G3 from '../assets/audio/G3.mp3';
+import C3 from '../assets/audio/C3.mp3';
+import E3 from '../assets/audio/E3.mp3';
+import A3 from '../assets/audio/A3.mp3';
 
 export default defineComponent({
   name: 'Tuner',
+  setup() {
+    const playG = () => {
+      new Audio(G3).play();
+    };
+
+    const playC = () => {
+      new Audio(C3).play();
+    };
+
+    const playE = () => {
+      new Audio(E3).play();
+    };
+
+    const playA = () => {
+      new Audio(A3).play();
+    };
+
+    return {
+      playG,
+      playC,
+      playE,
+      playA,
+    };
+  },
   components: {
     Button,
   },
